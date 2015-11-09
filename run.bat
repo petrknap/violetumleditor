11512069ff8fe98c8bc0854046cee532fc8e260d
Petr Knap <dev@petrknap.cz>
2015-11-09 20:23:52 +0100

    Run script

---
+++ run.bat
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GOTO Windows
+
+# Linux
+if [ "$1" != "" ]
+then
+    cd "$1"
+fi
+java -Dfile.encoding=UTF8 -jar ./2.0.0.jar
+exit
+
+:Windows
+if "%1" NEQ "" cd "%1"
+start javaw -Dfile.encoding=UTF8 -jar ./2.0.0.jar
+exit
