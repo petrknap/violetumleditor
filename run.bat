154ad12e844a29b43cc44055f2c96e82aa264366
Petr Knap <dev@petrknap.cz>
2016-12-12 08:43:42 +0100

    Updated from 2.0.0 to 2.1.0

--- run.bat
+++ run.bat
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Disable autocrlf before commit (git config core.autocrlf false)
+#
 
 GOTO Windows
 
@@ -7,10 +10,10 @@ if [ "$1" != "" ]
 then
     cd "$1"
 fi
-java -Dfile.encoding=UTF8 -jar ./2.0.0.jar
+java -Dfile.encoding=UTF8 -jar ./2.1.0.jar
 exit
 
 :Windows
 if "%1" NEQ "" cd "%1"
-start javaw -Dfile.encoding=UTF8 -jar ./2.0.0.jar
+start javaw -Dfile.encoding=UTF8 -jar ./2.1.0.jar
 exit
